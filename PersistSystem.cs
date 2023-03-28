65c340c24af71a8ef1506e91576de7bab7d3dde5
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-03-28 12:39:47 +0200

    Version 1.0.0

---
+++ PersistSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+/// <summary>
+/// Persist system class used to save and load JSON objects to files. 
+/// </summary>
+public class PersistSystem
+{
+    private string directoryPath;
+
+    public string DirectoryPath
+    {
+        get { return directoryPath; }
+        set { directoryPath = value; }
+    }
+
+    public PersistSystem(string directoryPath)
+    {
+        this.directoryPath = directoryPath;
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+    }
+
+    /// <summary>
+    /// Save a JSON object to a file.
+    /// </summary>
+    /// <param name="fileName">File name.</param>
+    /// <param name="json">JSON object that will be saved.</param>
+    public void SaveJSON(string fileName, string json)
+    {
+        string filePath = Path.Combine(directoryPath, fileName);
+
+        File.WriteAllText(filePath, json);
+    }
+
+    /// <summary>
+    /// Load a JSON object from a file.
+    /// </summary>
+    /// <param name="fileName">File name.</param>
+    /// <returns>JSON object.</returns>
+    public string LoadJSON(string fileName)
+    {
+        string filePath = Path.Combine(directoryPath, fileName);
+
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        throw new Exception("File not found: " + filePath);
+    }
+}
